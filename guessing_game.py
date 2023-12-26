b537a1eaddad7ad06d3e29c027a21b955de5c0ab
Bartosz Sławecki <bswck.dev@gmail.com>
2023-12-26 08:47:10 +0100

    Rename variables of guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,40 +1,49 @@
 # Split into multi-line for readability; this stmt expression can be fit into one line.
 
-# Usage: python -m guessing_game [-h] [-min MIN_NUMBER] [-max MAX_NUMBER] [-g GUESSES]
+# Usage: t.py [-h] [-min MIN_NUMBER] [-max MAX_NUMBER] [-g GUESSES]
 
 # Options:
 #   -h, --help                                  show this help message and exit
 #   -min MIN_NUMBER, --min-number MIN_NUMBER    minimum number
 #   -max MAX_NUMBER, --max-number MAX_NUMBER    maximum number
-#   -g GUESSES, --guesses GUESSES               number of guesses
+#   -g GUESSES, --max-guesses GUESSES           maximum number of guesses
 
-(p := __import__("argparse").ArgumentParser()) and (
-    p.add_argument("-min", "--min-number", type=int, default=1)
-) and (p.add_argument("-max", "--max-number", type=int, default=10)) and (
-    p.add_argument("-g", "--guesses", type=int, default=5)
-) and (ns := p.parse_args()) and ((kmin := ns.min_number), (kmax := ns.max_number)) and (
-    gmax := ns.guesses
-) and (
-    ask := lambda prompt: (
-        (ih := __import__("typing").cast("list[object]", [0]))
-        and (ii := iter(ih))
+(argument_parser := __import__("argparse").ArgumentParser()) and (
+    argument_parser.add_argument("-min", "--min-number", type=int, default=1)
+) and (argument_parser.add_argument("-max", "--max-number", type=int, default=10)) and (
+    argument_parser.add_argument("-g", "--max-guesses", type=int, default=5)
+) and (arguments := argument_parser.parse_args()) and (
+    (min_number := arguments.min_number),
+    (max_number := arguments.max_number),
+) and (max_guesses := arguments.max_guesses) and (
+    input_number := lambda prompt: (
+        (__input_helper := __import__("typing").cast("list[object]", [0]))
+        and (__input_iterator := iter(__input_helper))
         and [
             (
-                (a := input("Your guess: "))
-                and (a.isdigit() and ((g := int(a)) or True)) or ih.append(0)
+                (answer_candidate := input("Your guess: "))
+                and (
+                    answer_candidate.isdigit()
+                    and ((number_from_player := int(answer_candidate)) or True)
+                )
+                or __input_helper.append(0)
             )
-            for _ in ii
+            for _ in __input_iterator
         ]
-        and g
+        and number_from_player
     )
-) and (n := __import__("random").randint(kmin, kmax)) and (it := iter(r := (range(gmax)))) and (
-    log := [
-        g
+) and (random_number := __import__("random").randint(min_number, max_number)) and (
+    __game_iterator := iter(range(max_guesses))
+) and (
+    guesses_made := [
+        answered_number
         if (
-            (g := ask("What is your guess? ")) != n
-            and (print("Too low" if g < n else "Too high") or True)
+            (answered_number := input_number("What is your guess? ")) != random_number
+            and (print("Too low" if answered_number < random_number else "Too high") or True)
         )
-        else (print("You win!") or list(it))
-        for _ in it
+        else (print("You win!") or list(__game_iterator))
+        for _ in __game_iterator
     ]
-) and len(log) == len(r) and print(f"You ran out of guesses! The right number was {n}.")
+) and len(guesses_made) == max_guesses and answered_number != random_number and print(
+    f"You ran out of guesses! The right number was {random_number}."
+)
