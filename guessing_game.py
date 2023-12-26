9d9425787e93f7aa3d0cb7620514ad3d1a304c8f
Bartosz Sławecki <bswck.dev@gmail.com>
2023-12-26 08:37:13 +0100

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,33 @@
 # Split into multi-line for readability; this stmt expression can be fit into one line.
 
-(
+# Usage: t.py [-h] [-min MIN_NUMBER] [-max MAX_NUMBER] [-g GUESSES]
+
+# Options:
+#   -h, --help                                  show this help message and exit
+#   -min MIN_NUMBER, --min-number MIN_NUMBER    minimum number
+#   -max MAX_NUMBER, --max-number MAX_NUMBER    maximum number
+#   -g GUESSES, --guesses GUESSES               number of guesses
+
+(p := __import__("argparse").ArgumentParser()) and (
+    p.add_argument("-min", "--min-number", type=int, default=1)
+) and (p.add_argument("-max", "--max-number", type=int, default=10)) and (
+    p.add_argument("-g", "--guesses", type=int, default=5)
+) and (ns := p.parse_args()) and ((kmin := ns.min_number), (kmax := ns.max_number)) and (
+    gmax := ns.guesses
+) and (
     ask := lambda prompt: (
         (ih := __import__("typing").cast("list[object]", [0]))
         and (ii := iter(ih))
         and [
-            ((a := input("Your guess: ")) and ((a.isdigit() and (g := int(a))) or ih.append(0)))
+            (
+                (a := input("Your guess: "))
+                and (a.isdigit() and ((g := int(a)) or True)) or ih.append(0)
+            )
             for _ in ii
         ]
         and g
     )
-) and (n := __import__("random").randint(kmin := 1, kmax := 10)) and (it := iter(r := (range(kmin, kmax+1)))) and (
+) and (n := __import__("random").randint(kmin, kmax)) and (it := iter(r := (range(gmax)))) and (
     log := [
         g
         if (
