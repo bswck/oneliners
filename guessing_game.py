b1107e6c6b3a5d81b4692c7b032cab02b11f5d26
Bartosz Sławecki <bswck.dev@gmail.com>
2023-12-26 08:22:45 +0100

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,23 @@
+# Split into multi-line for readability; this stmt expression can be fit into one line.
+
+(
+    ask := lambda prompt: (
+        (ih := __import__("typing").cast("list[object]", [0]))
+        and (ii := iter(ih))
+        and [
+            ((a := input("Your guess: ")) and ((a.isdigit() and (g := int(a))) or ih.append(0)))
+            for _ in ii
+        ]
+        and g
+    )
+) and (n := __import__("random").randint(1, 10)) and (it := iter(range(atts := 10))) and (
+    log := [
+        g
+        if (
+            (g := ask("What is your guess? ")) != n
+            and (print("Too low" if g < n else "Too high") or True)
+        )
+        else (print("You win!") or list(it))
+        for _ in it
+    ]
+) and len(log) == atts and print(f"You ran out of guesses! The right number was {n}.")
